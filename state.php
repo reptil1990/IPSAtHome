3feb3f046b3a7cf23e308d5d21d45a13ee90dd16
reptil1990 <Carsten@reptile-mbp.speedport_w_921v_1_37_000>
2015-04-05 02:29:54 +0200

    Innitial Commit
    First Version for Beta Testing

---
+++ state.php
@@ -0,0 +1,75 @@
+<?PHP
+
+/*
+
+  PHP IPS Script
+  APP in Beta!
+
+  Created by DJ Reptile on 01.04.15.
+  Copyright (c) 2015 reptile. All rights reserved.
+
+
+ BEISPIEL URL bei ankommen: https://meine-inet-ip/state.php?name=MaxMustermann&state=true&s=123456789
+ BEISPIEL URL bei verlassen: https://meine-inet-ip/state.php?name=MaxMustermann&state=false&s=123456789
+
+ Die echo bitte nicht verändern, da die APP diese zur auswertung benutzt!
+
+ Das Script in das ROOT-Verzeichnis des IPS-Servers legen.
+ 
+ 
+*/
+
+
+// Hier einen Sicherheitscode vergeben, der als parameter mit übergeben wird!
+$secureCode = "123456789";
+
+
+
+if(isset($_GET['s']) && $_GET['s'] == $secureCode) 
+{
+
+
+				// Hier den Namen der Variablem eintragen
+  if($_GET['person'] == 'MaxMustermann') 
+
+	if ($_GET['state'] == 'true')
+		{
+			// BOOL in IPS und ID eintragen
+  			  SetValue(00000, true);	
+
+			//OPTIONAL: Push über die IPS APP
+		#    WFC_PushNotification(26497 /*[WebFront]*/, 'Home', 'NAME @Home', '', 0);
+		echo('arrived');
+		}
+		
+	else
+		{
+			 SetValue(0000, false);
+    		echo 'leaved';
+		}
+
+
+				// 2. Person möglich
+if($_GET['person'] == 'WIE HEIßt DIE 2te PERSON?') 
+{
+
+	if ($_GET['state'] == 'true')
+		{
+		
+			// BOOL in IPS und ID eintragen
+    		SetValue(00000, true);
+
+			//OPTIONAL: Push über die IPS APP
+		#    WFC_PushNotification(26497 /*[WebFront]*/, 'Home', 'Adeline @Home', '', 0);
+			echo('arrived');
+		}
+	else
+		{
+			SetValue(00000, false);
+    		echo 'leaved';
+		}
+
+}
+}
+
+?>
